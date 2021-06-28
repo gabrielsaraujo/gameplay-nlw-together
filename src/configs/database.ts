96b816181af3e353b6b505b2fe541f3e87475fc0
Gabriel Araujo <gabriellsaaraujo@gmail.com>
2021-06-28 01:47:13 -0300

    Criação do SignOut do usuário. Criado função para limpar os dados do Storage caso o usuário faça o Logout. Carregando os usuários logados no servidor dos agendamentos e ajustes gerais na aplicação.

---
+++ src/configs/database.ts
@@ -0,0 +1,10 @@
+const DATABASE_NAME = '@gameplay';
+
+//@nomeDoApp: collection
+const COLLECTION_USER = `${DATABASE_NAME}:user`;
+const COLLECTION_APPOINTMENT = `${DATABASE_NAME}:appointments`;
+
+export {
+    COLLECTION_USER,
+    COLLECTION_APPOINTMENT
+}
